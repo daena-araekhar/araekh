ab60e1d4df44f6663baca063eec307042896171f
daena-araekhar <araekhar.daena@gmail.com>
2024-03-16 04:22:59 -0300

    Com

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://daena-araekhar.github.io',
-  base:'theAraekhian',
   integrations: [tailwind(), mdx()]
 });
