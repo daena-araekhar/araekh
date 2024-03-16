36c257c7effbda95bb352d00cf45e4cd6c381f47
daena-araekhar <araekhar.daena@gmail.com>
2024-03-16 03:49:04 -0300

    Modificadas algunas weas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://daena-araekhar.github.io',
+  base:'AraekhiaWeb',
   integrations: [tailwind(), mdx()]
 });
